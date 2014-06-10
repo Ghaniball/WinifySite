a274225d4c02975bbd9f1137349dc2351196c1c9
igodorogea <igodorogea@winify.com>
2014-06-10 10:25:25 +0300

    Begin media queries adaptations

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,17 +11,17 @@ angular
     'winifySiteDirectives'
   ])
   .config(['getSearchKeyProvider', 'homePageBlocks',
-  function(getSearchKeyProvider, homePageBlocks){
-    getSearchKeyProvider.setBlocks(homePageBlocks);
-  }])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/home', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeCtrl',
-        reloadOnSearch: false
-      })
-      .otherwise({
-        redirectTo: '/home'
-      });
-  });
+    function(getSearchKeyProvider, homePageBlocks) {
+      getSearchKeyProvider.setBlocks(homePageBlocks);
+    }])
+  .config(['$routeProvider', function($routeProvider) {
+      $routeProvider
+        .when('/home', {
+          templateUrl: 'views/home.html',
+          controller: 'HomeCtrl',
+          reloadOnSearch: false
+        })
+        .otherwise({
+          redirectTo: '/home'
+        });
+    }]);
