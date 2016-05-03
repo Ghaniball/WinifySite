e36fe55b176b5edd7f0610af7bb4ad8abc5a6beb
Ivan Godorogea <igodorogea@winify.com>
2016-05-03 11:11:52 +0300

    remove phone number

--- Gruntfile.js
+++ Gruntfile.js
@@ -517,7 +517,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('serve', function (target) {
     if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
+      return grunt.task.run([/*'build', */'connect:dist:keepalive']);
     }
 
     grunt.task.run([
@@ -565,7 +565,7 @@ module.exports = function (grunt) {
     'uglify',
     'rev',
     'usemin',
-    'htmlmin',/*
+    'htmlmin'/*,
     'htmlSnapshot',
     'exec'*/
   ]);
